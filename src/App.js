c600d980e03402f690851455ecb8f963ec15132b
Vineeth Sai Ram <k.vineeth@iitg.ac.in>
2022-01-15 12:55:48 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Components/Layout/Navbar";
+import Footer from "./Components/Layout/Footer";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import About from "./Pages/AboutPage";
+import NotFound from "./Pages/NotFound";
+import Home from "./Pages/Home";
+import User from "./Pages/user";
+import {GithubProvider} from "./Context/Github/GithubContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GithubProvider>
+      <Router>
+        <div className= 'flex flex-col justify-between h-screen'>
+          <Navbar />
+            <Routes>
+              <Route exact path = '/' element = {<Home/>}/>
+                <Route path = '/about' element = {<About/>}/>
+                <Route path = '/user/:login' element = {<User/>}/>
+                <Route path = '/notFound' element = {<NotFound/>}/>
+                <Route path = '/*' element = {<NotFound/>}/>
+            </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
